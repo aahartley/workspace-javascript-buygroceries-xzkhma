1127a06b6cb193c28d1599746bd1b77952ef1dfc
aahartley <aahartley2000@gmail.com>
2020-11-04 13:16:41 -0500

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -3,27 +3,67 @@
  */
 
 // get all the hot class elements- change them to cool class
-$('li.hot').removeClass('hot');
-$('li').addClass('cool');
+$(".hot").each(function() {
+    $(this).removeClass("hot");
+    $(this).addClass("cool");
+  });
+
 
 
 // traverse the elements
+$("#one").next().next().text("milk");
 
 
 // add a new element
+//$("#todo").append("<li class='cool'>hello</li>")
 
 
 // add by clicking the plus sign
-document.getElementById("add").addEventListener("click", addElement);
+//document.getElementById("add").addEventListener("click", addElement);
+$('#add').click(addElement);
+
 
 function addElement() {
   // add a new element
-  
+      // add a input text box
+      $("#todo").append("<li><input type='text'> </li>");
+
+      // whenever the user are done
+     
+      $('input').blur(function(){
+        //change style
+        $(this).parent().addClass("cool");
+
+        // bind click with new li element 
+               $(this).parent().addClass("cool");
+
+        //remove input element and replace with the value user typed
+         var userinput = $(this).val();
+        $(this).parent().text(userinput);
+      });
 }
+
+$("li").click(changeStyle);  // bind click with the event handler
 
 
 //  click the li element will change the changeStyle
 function changeStyle() {
+  if($(this).hasClass('complete')){
+    $(this).removeClass('complete');
+    $(this).addClass('cool');
+  }else{
+  if($(this).hasClass('cool')){
+    $(this).removeClass('cool');
+    $(this).addClass('complete');
+  }   
+  if($(this).hasClass('hot')){
+    $(this).removeClass('hot');
+    $(this).addClass('complete');
+  }
+  else{
+    $(this).addClass('complete');
+  }
+  }
 
 }
 
@@ -32,6 +72,7 @@ document.getElementById("remove").addEventListener("click", removeElement);
 
 function removeElement() {
   // remove the marked element
- 
+    $('li.complete').remove();
+
  
 }
