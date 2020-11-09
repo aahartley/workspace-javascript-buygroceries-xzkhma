6b2ee4809297ca04c515297ac14509c4b0aee921
aahartley <aahartley2000@gmail.com>
2020-11-09 13:12:12 -0500

    1 file modified

--- index.js
+++ index.js
@@ -17,7 +17,15 @@ import * as firebaseui from "firebaseui";
 
 // Add Firebase project configuration object here
 var firebaseConfig = {
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  apiKey: "AIzaSyBoJ6-2VetXjIyotWfLsMCw6Vcd8dDKfqc",
+  authDomain: "buygroceries-7fcb9.firebaseapp.com",
+  databaseURL: "https://buygroceries-7fcb9.firebaseio.com",
+  projectId: "buygroceries-7fcb9",
+  storageBucket: "buygroceries-7fcb9.appspot.com",
+  messagingSenderId: "345609131270",
+  appId: "1:345609131270:web:894f01ad228a43c4d0ba87",
+  measurementId: "G-BYXJRM5H7J"
 };
 
 firebase.initializeApp(firebaseConfig);
